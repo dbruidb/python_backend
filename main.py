83c2653b04a0932f22d7ea186cd302e8c8553f5c
Ontaneda Adrian <dbruid98760@gmail.com>
2024-05-28 12:28:20 -0500

    4 intento

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
 
 from fastapi import FastAPI
+import uvicorn
+
+if __name__ == "__main__":
+    uvicorn.run("main:app", host="0.0.0.0", port=80, log_level="info")
 
 app = FastAPI()
-
 
 @app.get("/")
 def read_root():
