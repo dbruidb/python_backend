dcf4da6aa0ee9fca2411aed4ee6c64bf5d1586bd
Ontaneda Adrian <dbruid98760@gmail.com>
2024-06-01 14:55:42 -0500

    hola

--- main.py
+++ main.py
@@ -8,9 +8,6 @@ import asyncpg
 
 from  routes import general
 
-# Emitar la app en 0.0.0.0:80
-if __name__ == "__main__":
-    uvicorn.run("main:app", host="0.0.0.0", port=80, log_level="info")
 
 
 #la app a ser emitida
